d1f7a29e6dd113a2e4bf9a4af9d78564f4e15fb5
urbancloset <urbancloset4321@gmail.com>
2022-01-13 11:43:07 +0530

    1st

---
+++ app/src/main/java/com/example/loginscreen/singupscreen.kt
@@ -0,0 +1,11 @@
+package com.example.loginscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class singupscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_singupscreen)
+    }
+}
