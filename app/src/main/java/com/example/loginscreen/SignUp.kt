c548c80d144ec15594ea7db19c12ddbd5670f22e
urbancloset <urbancloset4321@gmail.com>
2022-01-17 13:12:00 +0530

    Signup Activity Changed Using Material UI(Vivek)

--- app/src/main/java/com/example/loginscreen/SignUp.kt
+++ app/src/main/java/com/example/loginscreen/SignUp.kt
@@ -7,5 +7,6 @@ class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        supportActionBar?.hide()
     }
 }
