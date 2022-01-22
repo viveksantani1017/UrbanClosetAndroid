8b74438c9ad525bc15bd6a468abb3953f981555b
urbancloset <urbancloset4321@gmail.com>
2022-01-22 13:29:59 +0530

    Added Main activity_main.xml(Vivek)

--- app/src/main/java/com/example/loginscreen/MainActivity.kt
+++ app/src/main/java/com/example/loginscreen/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.loginscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.synnapps.carouselview.CarouselView
+import com.synnapps.carouselview.ImageListener
 
 class MainActivity : AppCompatActivity() {
+    var sampleImages = intArrayOf(
+        R.drawable.menbanner,
+        R.drawable.womenbanner,
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val carouselView = findViewById(R.id.carouselView) as CarouselView;
+        carouselView.setPageCount(sampleImages.size);
+        carouselView.setImageListener(imageListener);
+
+        }
+        var imageListener: ImageListener = object : ImageListener {
+            override fun setImageForPosition(position: Int, imageView: ImageView) {
+                // You can use Glide or Picasso here
+                imageView.setImageResource(sampleImages[position])
     }
+}
 }
