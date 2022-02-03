143361c61ebab5aae5ee1bf55d3212da676841d5
urbancloset <urbancloset4321@gmail.com>
2022-02-03 11:56:24 +0530

    added wishlist icon

---
+++ app/src/main/java/com/example/loginscreen/productlist.kt
@@ -0,0 +1,11 @@
+package com.example.loginscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class productlist : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_productlist)
+    }
+}
