7e7608b61284dc00ef0a0de224357d72a4f77849
urbancloset <urbancloset4321@gmail.com>
2022-02-08 12:11:53 +0530

    MADE TEST ACTIVITY (VIVEK)

---
+++ app/src/main/java/com/example/loginscreen/TestActivity.kt
@@ -0,0 +1,11 @@
+package com.example.loginscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.product_grid)
+    }
+}
