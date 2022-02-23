41a5757de7ca42824f238d86956e6dd65c2325cc
urbancloset <urbancloset4321@gmail.com>
2022-02-23 15:59:18 +0530

    added a whole lot of things

---
+++ app/src/main/java/com/example/loginscreen/adapters/OrderDetailAdapter.kt
@@ -0,0 +1,64 @@
+package com.example.loginscreen.adapters
+
+import android.app.Activity
+import android.net.Uri
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.loginscreen.R
+import com.example.loginscreen.models.orderdetails
+
+class OrderDetailAdapter(
+        private val activity: Activity,
+        private val objects: Array<orderdetails>
+    ) : ArrayAdapter<orderdetails>(activity, R.layout.orderdetails_grid, objects)
+    {
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View
+        {
+            var view: View? = convertView
+            val viewHolder: ViewHolder
+
+            if (view == null)
+            {
+                view = activity.layoutInflater.inflate(R.layout.orderdetails_grid, parent, false)
+
+                viewHolder = ViewHolder()
+                viewHolder.image = view.findViewById(R.id.productimage)
+                viewHolder.name = view.findViewById(R.id.productname)
+                viewHolder.size = view.findViewById(R.id.sizevalue)
+                viewHolder.totalprice = view.findViewById(R.id.totalpricevalue)
+                viewHolder.quantity = view.findViewById(R.id.quantityvalue)
+                view.tag = viewHolder
+            }
+            else
+            {
+                viewHolder = view.tag as ViewHolder
+            }
+
+            viewHolder.orderedproductid.text = objects[position].productName
+            viewHolder.image.setImageURI(Uri.parse("${activity.externalCacheDir}/images/${objects[position].productimage}"))
+            viewHolder.name.text = objects[position].productName
+            viewHolder.size.text = objects[position].size
+            viewHolder.totalprice.text = objects[position].productimage
+            viewHolder.quantity.text = objects[position].quantity.toString()
+
+            view?.contentDescription = objects[position].id.toString()
+
+            return view!!
+        }
+
+        companion object
+        {
+            class ViewHolder
+            {
+                lateinit var name: TextView
+                lateinit var size: TextView
+                lateinit var orderedproductid: TextView
+                lateinit var totalprice: TextView
+                lateinit var quantity: TextView
+                lateinit var image: ImageView
+            }
+        }
+    }
---
+++ app/src/main/java/com/example/loginscreen/models/orderdetails.kt
@@ -0,0 +1,11 @@
+package com.example.loginscreen.models
+
+class orderdetails(
+    var id: Int,
+    var orderedproductid: Int,
+    var productimage: String,
+    var productName: String,
+    var size: String,
+    var quantity: Int,
+    var totalPrice: Int
+)
