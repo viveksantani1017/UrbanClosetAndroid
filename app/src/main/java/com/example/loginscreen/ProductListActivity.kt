8b3516e7cb8a93100a7871b5e6eba4621462d770
urbancloset <urbancloset4321@gmail.com>
2022-02-26 14:20:04 +0530

    UPDATED PROJECT WITH ADDED ALERT BOX

--- app/src/main/java/com/example/loginscreen/ProductListActivity.kt
+++ app/src/main/java/com/example/loginscreen/ProductListActivity.kt
@@ -2,58 +2,50 @@ package com.example.loginscreen
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import android.widget.GridView
-import android.widget.ImageButton
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.loginscreen.adapters.ProductGridAdapter
 import com.example.loginscreen.api.Productapi
-import com.example.loginscreen.models.Category
-import com.example.loginscreen.models.Product
 import kotlinx.coroutines.*
 
 class ProductListActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_list)
-        var actionBar = getSupportActionBar()
-        val catintent = intent
-        val catid = catintent.getIntExtra("CatId",0)
-        actionBar?.setTitle(catintent.getStringExtra("CategoryName"))
-        actionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        val categoryId = intent.getIntExtra("CatId", 0)
+
+        supportActionBar?.title = intent.getStringExtra("CategoryName")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         val grdProducts = findViewById<GridView>(R.id.grdProducts)
-        var icon = findViewById<ImageView>(R.id.icon)
-        icon = null
-        val uri = "@drawable/ic_fav"
-        var imgres = resources.getIdentifier(uri,null,packageName)
-        var res = resources.getDrawable(imgres)
+
         CoroutineScope(Dispatchers.IO).launch {
             grdProducts.setOnItemClickListener { _, view, _, _ ->
+
                 val productId = view.contentDescription.toString().toInt()
                 val intent = Intent(this@ProductListActivity, productdetails::class.java)
                 intent.putExtra("ProductID", productId)
+
                 CoroutineScope(Dispatchers.Main).launch {
                     startActivity(intent)
-
                 }
             }
 
-            val products = Productapi.getAll(catid,this@ProductListActivity)
+            val products = Productapi.getAll(categoryId, this@ProductListActivity)
             if (products.isNotEmpty()) {
 
-                for (product in products){
+                for (product in products) {
                     Productapi.downloadImage(this@ProductListActivity, product)
-                    if(product.inwishlist)
-                    {
-                       icon?.setBackgroundResource(R.drawable.ic_fav)
-                    }
                 }
 
                 val adapter = ProductGridAdapter(this@ProductListActivity, products)
                 withContext(Dispatchers.Main) { grdProducts.adapter = adapter }
+
             } else {
                 withContext(Dispatchers.Main) {
                     Toast.makeText(
@@ -65,7 +57,6 @@ class ProductListActivity : AppCompatActivity() {
                 }
             }
         }
-
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
